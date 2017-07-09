5042d20e71d3ad51b2c871bb1880d3d821f8b928
Constant Dupuis <constant.dupuis@gmail.com>
2017-07-09 08:41:46 +0200

    use my own classes to play with classes hierarchies, Base, DerivedA, DerivedB...

--- TourCpp_class_hierarchy/TourCpp_class_hierarchy.cpp
+++ TourCpp_class_hierarchy/TourCpp_class_hierarchy.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+// sample from book
+
 class Point {
 public:
 	Point() : x{ 0 }, y{0} {}
@@ -42,9 +44,84 @@ private:
 };
 
 
+// simplified version to play with
+
+class Base {
+public:
+	Base(string n) : name{ n } { cout << "Base [" << name << "]\n"; }
+	Base() : Base("CX") {}
+	void virtual methodA() = 0;
+	void virtual methodB() { cout << "Base::methodB [" << name << "]\n"; }
+
+	~Base() { cout << "~Base [" << name << "]\n"; }
+
+protected:
+	string name;
+};
+
+class DerivedA : public Base {
+public:
+	DerivedA(string n) : Base(n) { cout << "DerivedA [" << name << "]\n"; }
+	void methodA() { cout << "DerivedA::methodA [" << name << "]\n"; }
+	//void methodB() { cout << "DerivedA::methodB [" << name << "]\n"; }
+
+	~DerivedA() { cout << "~DerivedA [" << name << "]\n"; }
+};
+
+class DerivedB : public DerivedA {
+public:
+	DerivedB() : DerivedA("DB") { cout << "DerivedB [" << name << "]\n"; }
+
+	void methodB() { 
+		// call ancestors methodB, in this case DerivedA and base are the same, because DevivedA dosn't override methodB
+		cout << ">> Call ancestor methods\n\t";
+		DerivedA::methodB();
+		cout << "\t";
+		Base::methodB();
+		cout << "DerivedB::methodB [" << name << "]\n"; 
+	}
+
+	~DerivedB() { cout << "~DerivedB [" << name << "]\n"; }
+};
+
+class DerivedC : public Base {
+public:
+	DerivedC() {} // should call Base()
+
+	void methodA() { cout << "DerivedC::methodA [" << name << "]\n"; }
+
+};
+
 int main()
 {
-	Circle c(Point(), 50);
+	//Circle c(Point(), 50);
+	DerivedA DA("DA");
+
+	DA.methodA();
+	DA.methodB();
+
+	DerivedB DB;
+
+	DB.methodA();
+	DB.methodB();
+
+	DerivedC DC;
+
+	DC.methodA();
+	DC.methodB();
+
+	cout << ">> Dynamic cast to base* \n";
+	Base* b = dynamic_cast<Base*>(&DC);
+	if (b)
+	{
+		cout << "\t";
+		b->methodA();
+		cout << "\t";
+		b->methodB();
+	}
+	cout << ">> Dynamic cast to base* \n";
+		
+
     return 0;
 }
 
